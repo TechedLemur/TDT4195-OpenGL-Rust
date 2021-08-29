fa6c532d39955f94d3299def4cd14de3ff2f9901
Jostein Tysse <josteiht@stud.ntnu.no>
2021-08-29 21:10:49 +0200

    Add functions for each subtask triangle

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,46 @@ fn offset<T>(n: u32) -> *const c_void {
 // Get a null pointer (equivalent to an offset of 0)
 // ptr::null()
 
+fn task1_triangles() -> (Vec<f32>, Vec<u32>) {
+    // Have separate objects for each triangle to make it easier to read and modify
+    let triangle1: Vec<f32> = vec![-0.2, -0.2, 0.0, 0.2, -0.1, 0.0, 0.0, 0.5, 0.0];
+    let triangle2: Vec<f32> = vec![-0.9, -0.6, 0.0, -0.5, -0.6, 0.0, -0.7, 0.2, 0.0];
+    let triangle3: Vec<f32> = vec![-0.8, 0.6, 0.0, -0.7, 0.4, 0.0, -0.6, 0.6, 0.0];
+    let triangle4: Vec<f32> = vec![0.7, -0.5, 0.0, 0.9, -0.3, 0.0, 0.7, -0.1, 0.0];
+    let triangle5: Vec<f32> = vec![0.25, 0.25, 0.0, 0.95, 0.7, 0.0, 0.3, 0.8, 0.0];
+    let triangle6: Vec<f32> = vec![0.0, -0.6, 0.0, 0.5, -0.7, 0.0, 0.3, -0.5, 0.0];
+    let mut coordinates_task1: Vec<f32> = Vec::new();
+    coordinates_task1.extend(&triangle1);
+    coordinates_task1.extend(&triangle2);
+    coordinates_task1.extend(&triangle3);
+    coordinates_task1.extend(&triangle4);
+    coordinates_task1.extend(&triangle5);
+    coordinates_task1.extend(&triangle6);
+
+    let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+    return (coordinates_task1, indices);
+}
+
+fn task2_triangle() -> (Vec<f32>, Vec<u32>) {
+    let triangle_task_2a: Vec<f32> = vec![0.6, -0.8, -1.2, 0.0, 0.4, 0.0, -0.8, -0.2, 1.2];
+    let indices: Vec<u32> = vec![0, 1, 2];
+    return (triangle_task_2a, indices);
+}
+
+fn task2b_triangles() -> (Vec<f32>, Vec<u32>) {
+    // Have separate objects for each triangle to make it easier to read and modify
+    let triangle1: Vec<f32> = vec![-0.2, -0.2, 0.0, 0.2, -0.1, 0.0, 0.0, 0.5, 0.0];
+    let triangle2: Vec<f32> = vec![-0.9, -0.6, 0.0, -0.5, -0.6, 0.0, -0.7, 0.2, 0.0];
+
+    let mut coordinates_task1: Vec<f32> = Vec::new();
+    coordinates_task1.extend(&triangle1);
+    coordinates_task1.extend(&triangle2);
+
+    let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5];
+
+    return (coordinates_task1, indices);
+}
 // == // Modify and complete the function below for the first task
 unsafe fn set_up_vao(coordinates: &Vec<f32>, indices: &Vec<u32>) -> u32 {
     let mut array: u32 = 0;
@@ -136,25 +176,10 @@ fn main() {
         // == // Set up your VAO here
         let vao_id: u32;
 
-        // Have separate objects for each triangle to make it easier to read and modify
-        let triangle1: Vec<f32> = vec![-0.2, -0.2, 0.0, 0.2, -0.1, 0.0, 0.0, 0.5, 0.0];
-        let triangle2: Vec<f32> = vec![-0.9, -0.6, 0.0, -0.5, -0.6, 0.0, -0.7, 0.2, 0.0];
-        let triangle3: Vec<f32> = vec![-0.8, 0.6, 0.0, -0.7, 0.4, 0.0, -0.6, 0.6, 0.0];
-        let triangle4: Vec<f32> = vec![0.7, -0.5, 0.0, 0.9, -0.3, 0.0, 0.7, -0.1, 0.0];
-        let triangle5: Vec<f32> = vec![0.25, 0.25, 0.0, 0.95, 0.7, 0.0, 0.3, 0.8, 0.0];
-        let triangle6: Vec<f32> = vec![0.0, -0.6, 0.0, 0.5, -0.7, 0.0, 0.3, -0.5, 0.0];
-        let mut coordinates_task1: Vec<f32> = Vec::new();
-        coordinates_task1.extend(&triangle1);
-        coordinates_task1.extend(&triangle2);
-        coordinates_task1.extend(&triangle3);
-        coordinates_task1.extend(&triangle4);
-        coordinates_task1.extend(&triangle5);
-        coordinates_task1.extend(&triangle6);
-
-        let indices: Vec<u32> = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+        let (coordinates, indices) = task2b_triangles();
 
         unsafe {
-            vao_id = set_up_vao(&coordinates_task1, &indices);
+            vao_id = set_up_vao(&coordinates, &indices);
         }
 
         // Basic usage of shader helper:
